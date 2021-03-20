14d1d03ea650506f7d7ba6cc021fe80111bbf6ad
mbabazisuzanne <mbabazisuzanne@gmail.com>
2021-03-20 18:00:16 +0300

    Added a route for the register file

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 //instantiating express
 const app = express(); 
 
+app.use(express.urlencoded({extended: true}))
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+app.get('/register',(req,res)=>{res.render('register')})
 app.listen(3000, ()=>{
     console.log('Listening on port 3000');
 })
