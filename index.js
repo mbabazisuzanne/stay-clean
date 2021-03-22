b2091f33fbec4babc9c555d226af195d0966c2df
mbabazisuzanne <mbabazisuzanne@gmail.com>
2021-03-22 23:43:13 +0300

    Added login and register routes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+//Dependencies
 const express = require('express');
+const registrationRoutes = require('./routes/registrationRoutes'); 
+const loginRoutes = require('./routes/loginRoutes');
+
 //instantiating express
 const app = express(); 
+
 
 app.use(express.urlencoded({extended: true}))
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+//middleware for serving static files 
 app.use(express.static('public'));
-app.get('/register',(req,res)=>{res.render('register')});
-app.get('/login',(req,res)=>{res.render('login')});
+
+//Routes
+app.use('/register',registrationRoutes); 
+app.use('/login',loginRoutes);
+
+// app.get('/register',(req,res)=>{res.render('register')});
+// app.get('/login',(req,res)=>{res.render('login')});
 app.listen(3000, ()=>{
     console.log('Listening on port 3000');
 })
