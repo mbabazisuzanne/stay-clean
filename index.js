0cc3d6f75183c81baca24d2f65654d2678dd1054
mbabazisuzanne <mbabazisuzanne@gmail.com>
2021-03-22 08:56:47 +0300

    Extended layout file in login and register

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
-app.get('/register',(req,res)=>{res.render('register')})
+app.get('/register',(req,res)=>{res.render('register')});
+app.get('/login',(req,res)=>{res.render('login')});
 app.listen(3000, ()=>{
     console.log('Listening on port 3000');
 })
