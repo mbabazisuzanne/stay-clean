8e29b5c52b6c3d2b3c949f208eb2f82bbad37e02
mbabazisuzanne <mbabazisuzanne@gmail.com>
2021-03-28 23:17:45 +0300

    Server successfully connected to database

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //Dependencies
 const express = require('express');
-require ('dotenv').config();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 const registrationRoutes = require('./routes/registrationRoutes'); 
 const loginRoutes = require('./routes/loginRoutes');
@@ -10,12 +12,30 @@ const addEmployees = require('./routes/addEmployees');
 //instantiating express
 const app = express(); 
 
+//database connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  
+  mongoose.connection
+    .on('open', () => {
+      console.log('Mongoose connection open');
+    })
+    .once('error', (err) => {
+      console.log(`Connection error: ${err.message}`);
+    });
 
+//middleware    
 app.use(express.urlencoded({extended: true}))
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+// app.use((req, res, next) => {
+//     console.log('a new request received at ' + Date.now());
+//     next();
+//   });
 //middleware for serving static files 
 app.use(express.static('public'));
 
@@ -23,7 +43,6 @@ app.use(express.static('public'));
 app.use('/register',registrationRoutes); 
 app.use('/login',loginRoutes);
 app.use('/addEmployees',addEmployees);
-
 
 // app.get('/register',(req,res)=>{res.render('register')});
 // app.get('/login',(req,res)=>{res.render('login')});
--- routes/addEmployees.js
+++ routes/addEmployees.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const employee = require('../models/employee')
 var multer = require('multer');
 var upload = multer({dest:'uploads/'});
 
+//gets employee
 router.get('/addEmployees', (req,res) => {
     res.render('addEmployees',{title:'Create Employee'} )
+})
+
+//gets employee list
+router.get('/', async (req, res) => { 
+    try{
+        //find all data in database 
+        const employeeDetails = await Employee.find();
+        res.render('employeeList', {users:employeeDetails, title: 'EmployeeList'});
+       
+    }catch(err){
+        res.send('Failed to retireve Employee Details')
+    }
+});
+
+router.post('/addEmployees', upload.single('imageupload'), (req, res) => {
+    try {
+        console.log(req.body)
+        // res.send(req.file);
+    } catch (err) {
+        res.send(400);
+        console.log("failed")
+    }
 })
 //image upload
 var storage = multer.diskStorage({
@@ -17,22 +41,31 @@ var storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage })
 
-router.post('/addEmployees', upload.single('imageupload'), (req, res) => {
-    try {
-        console.log(req.body)
-        // res.send(req.file);
-    } catch (err) {
-        res.send(400);
-    }
-})
-router.post('/createEmployee', upload.single('imageupload'), (req, res) => {
+// router.post('/addEmployees', upload.single('imageupload'), (req, res) => {
+//     const employee = new Employee(req.body);
+//     employee.imageupload = req.file.path;
+//     employee.save()
+//     .then(() => { res.send('Registered successfully!')})
+//     .catch((err) => {
+//         console.log(err);
+//         res.send('Sorry! Something went wrong.');
+//     })
+// })
+router.post('/addEmployees', upload.single('imageupload'), async (req, res) => {
+try{
+    console.log(req.body);
     const employee = new Employee(req.body);
-    employee.imageupload = req.file.path;
-    employee.save()
-    .then(() => { res.send('Registered successfully!')})
-    .catch((err) => {
-        console.log(err);
-        res.send('Sorry! Something went wrong.');
-    })
+  
+employee.imageupload = req.file.path;
+    //await code performing database operation 
+    await employee.save()
+    
+    res.redirect('/employee')
+} catch(err){
+
+    console.log(err);
+    res.send('Sorry! Something went wrong.');
+}
+
 })
 module.exports = router;
